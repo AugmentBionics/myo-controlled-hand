d6f14465007f574e24466ec28fe9282b7de3843d
prototriangle <prototriangle@gmail.com>
2018-02-14 17:11:33 +0000

    Add main arduino project file

---
+++ myo-controlled-hand.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+#include "Config.h"
+#include "MotorController.h"
+
+int buttonPin = BUTTON_PIN;
+
+void setup() {
+  MotorController mc = MotorController();
+
+}
+
+void loop() {
+}

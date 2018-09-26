fababe39e9269709d9297c5f3820c184adccf36e
prototriangle <prototriangle@gmail.com>
2018-09-26 17:35:36 +0100

    Update config

--- MyoControlledHand/Config.h
+++ MyoControlledHand/Config.h
@@ -1,12 +1,16 @@
 #ifndef Config_h
 #define Config_h
+#include "Arduino.h"
 
-#define BUTTON_PIN 11
-#define MOTOR_1_PIN 5
-#define MOTOR_2_PIN 6
-#define MOTOR_3_PIN 7
+#define BUTTON_1_PIN 2
+#define BUTTON_2_PIN 3
+#define MOTOR_1_PIN 11
+#define MOTOR_2_PIN 9
+#define MOTOR_3_PIN 6
+#define MYO_PIN A0
+#define POT_PIN A1
 #define NUMBER_OF_ACTUATORS 3
 #define NUMBER_OF_TOUCH_SENSORS 1
-#define NUMBER_OF_PRIMARY_GRIPS 5
+#define NUMBER_OF_PRIMARY_GRIPS 3
 
 #endif // ifndef Config_h

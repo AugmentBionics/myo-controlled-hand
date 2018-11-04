da4b152f76dcdc8a95febe79407cd4244301b1e1
prototriangle <prototriangle@gmail.com>
2018-11-04 17:02:14 +0000

    Update pin configuration

--- MyoControlledHand/Config.h
+++ MyoControlledHand/Config.h
@@ -2,15 +2,35 @@
 #define Config_h
 #include "Arduino.h"
 
-#define BUTTON_1_PIN 2
-#define BUTTON_2_PIN 3
-#define MOTOR_1_PIN 11
-#define MOTOR_2_PIN 9
-#define MOTOR_3_PIN 6
-#define MYO_PIN A0
-#define POT_PIN A1
-#define NUMBER_OF_ACTUATORS 3
-#define NUMBER_OF_TOUCH_SENSORS 1
-#define NUMBER_OF_PRIMARY_GRIPS 3
+#define MOTOR_1_FWD_PIN 2
+#define MOTOR_1_REV_PIN 3
+#define MOTOR_1_CUR_PIN 6
+
+#define MOTOR_2_FWD_PIN 7
+#define MOTOR_2_REV_PIN 8
+#define MOTOR_2_CUR_PIN 9
+
+#define MOTOR_3_FWD_PIN 10
+#define MOTOR_3_REV_PIN 11
+#define MOTOR_3_CUR_PIN 12
+
+#define MOTOR_4_FWD_PIN 13
+#define MOTOR_4_REV_PIN A0
+#define MOTOR_4_CUR_PIN A1
+
+#define MOTOR_5_FWD_PIN A2
+#define MOTOR_5_REV_PIN A3
+#define MOTOR_5_CUR_PIN A4
+
+#define SDA_PIN 4
+#define SCL_PIN 5
+
+#define MYO_1_PIN A5
+#define MYO_2_PIN A6
+#define POT_PIN A7
+
+
+#define NUMBER_OF_ACTUATORS 5
+#define NUMBER_OF_PRIMARY_GRIPS 4
 
 #endif // Config_h

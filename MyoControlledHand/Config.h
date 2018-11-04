1ffa08dbf7faaf5b0621cb5ec6bd76c579fac0cd
prototriangle <prototriangle@gmail.com>
2018-11-04 21:20:50 +0000

    Update pin configuration for 2-arduino system

--- MyoControlledHand/Config.h
+++ MyoControlledHand/Config.h
@@ -4,30 +4,29 @@
 
 #define MOTOR_1_FWD_PIN 2
 #define MOTOR_1_REV_PIN 3
-#define MOTOR_1_CUR_PIN 6
+#define MOTOR_1_CUR_PIN 4
 
-#define MOTOR_2_FWD_PIN 7
-#define MOTOR_2_REV_PIN 8
-#define MOTOR_2_CUR_PIN 9
+#define MOTOR_2_FWD_PIN 5
+#define MOTOR_2_REV_PIN 6
+#define MOTOR_2_CUR_PIN 7
 
-#define MOTOR_3_FWD_PIN 10
-#define MOTOR_3_REV_PIN 11
-#define MOTOR_3_CUR_PIN 12
+#define MOTOR_3_FWD_PIN 8
+#define MOTOR_3_REV_PIN 9
+#define MOTOR_3_CUR_PIN 10
 
-#define MOTOR_4_FWD_PIN 13
-#define MOTOR_4_REV_PIN A0
-#define MOTOR_4_CUR_PIN A1
+#define MOTOR_4_FWD_PIN 11
+#define MOTOR_4_REV_PIN 12
+#define MOTOR_4_CUR_PIN 13
 
-#define MOTOR_5_FWD_PIN A2
-#define MOTOR_5_REV_PIN A3
-#define MOTOR_5_CUR_PIN A4
+#define MOTOR_5_FWD_PIN A0
+#define MOTOR_5_REV_PIN A1
+#define MOTOR_5_CUR_PIN A2
+#define MOTOR_LIM_PIN A3
 
-#define SDA_PIN 4
-#define SCL_PIN 5
-
-#define MYO_1_PIN A5
-#define MYO_2_PIN A6
-#define POT_PIN A7
+#define MYO_1_SIG_PIN A4
+#define MYO_1_RAW_PIN A4
+#define MYO_2_SIG_PIN A5
+#define MYO_2_RAW_PIN A6
 
 
 #define NUMBER_OF_ACTUATORS 5

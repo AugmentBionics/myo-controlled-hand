d7e3cefd8147d2c0f1d4fb29ba47156802d2de82
prototriangle <prototriangle@gmail.com>
2018-11-05 00:07:33 +0000

    Add hacked together input classification

--- MyoControlledHand/MyoInput/MyoInput.cpp
+++ MyoControlledHand/MyoInput/MyoInput.cpp
@@ -1,5 +1,86 @@
-//
-// Created by alex on 04/11/18.
-//
-
+#include "Arduino.h"
 #include "MyoInput.h"
+#include "Config.h"
+
+MyoInput::MyoInput() {
+}
+
+float range(int *buffer, unsigned int length) {
+    int minVal = UINT16_MAX;
+    int maxVal = 0;
+    for (int i = 0; i < length; ++i) {
+        minVal = min(minVal, buffer[i]);
+        maxVal = max(maxVal, buffer[i]);
+    }
+    return abs(maxVal - minVal);
+}
+
+MyoInput::Action MyoInput::readAction() {
+    static unsigned int i = 0;
+
+    float t1 = 375;
+    float t2 = 380;
+//    float t1 = 385;
+//    float t2 = 385;
+
+    _m1Raw = analogRead(MYO_1_RAW_PIN);
+    _m1Sig = analogRead(MYO_1_SIG_PIN);
+    _m2Raw = analogRead(MYO_2_RAW_PIN);
+    _m2Sig = analogRead(MYO_2_SIG_PIN);
+
+    _m1RawBuffer[i] = _m1Raw;
+    _m2RawBuffer[i] = _m2Raw;
+
+    i = (i + 1) % bufferSize;
+
+    float r1 = range(_m1RawBuffer, bufferSize);
+    float r2 = range(_m2RawBuffer, bufferSize);
+
+    bool b1 = r1 > t1;
+    bool b2 = r2 > t2;
+
+    float d1 = r1 - t1;
+    float d2 = r2 - t2;
+
+    if (b1 && (r1 - r2) > 25) {
+        Serial.print("Close\t");
+        Serial.println(d1);
+        return MyoInput::close;
+    } else if (b2 && (r2 - r1) > 25) {
+        Serial.print("Open\t");
+        Serial.println(d2);
+        return MyoInput::open;
+    } else {
+        Serial.print(d1);
+        Serial.print('\t');
+        Serial.println(d2);
+        return MyoInput::none;
+    }
+
+//    Serial.print(_m1Raw);
+//    Serial.print(",");
+//    Serial.print(_m1Sig);
+//    Serial.print(",");
+//    Serial.print(_m2Raw);
+//    Serial.print(",");
+//    Serial.print(_m2Sig);
+//    Serial.print(",");
+//    Serial.print(range(_m1RawBuffer, bufferSize));
+//    Serial.print(",");
+//    Serial.print(range(_m2RawBuffer, bufferSize));
+//    Serial.print(",");
+//    Serial.print(0);
+//    Serial.print(",");
+//    Serial.print(1023);
+//    Serial.print("\n");
+
+    return none;
+}
+
+void MyoInput::init() {
+    pinMode(MYO_1_RAW_PIN, INPUT);
+    pinMode(MYO_1_SIG_PIN, INPUT);
+    pinMode(MYO_2_RAW_PIN, INPUT);
+    pinMode(MYO_2_SIG_PIN, INPUT);
+}
+

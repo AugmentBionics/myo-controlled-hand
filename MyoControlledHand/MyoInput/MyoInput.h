6422965611011f611ad2de81360b3b2514b36d88
prototriangle <prototriangle@gmail.com>
2019-02-04 02:48:32 +0000

    Add basic beginnings of doxygen documentation

--- MyoControlledHand/MyoInput/MyoInput.h
+++ MyoControlledHand/MyoInput/MyoInput.h
@@ -1,20 +1,25 @@
 #ifndef MYOCONTROLLEDHAND_MYOINPUT_H
 #define MYOCONTROLLEDHAND_MYOINPUT_H
 
+/*!
+ * Class that reads from myo sensors and interprets this as high-level "actions"
+ * TODO: Improve action detection
+ * TODO: Increase sensor count
+ */
 class MyoInput {
  public:
 
     MyoInput();
 
-    void init();
+    void init(); /*!< Setup pins */
 
     enum Action {
-        open,
-        close,
-        none
-    };
+        open, /*!< User intends to open the grip */
+        close, /*!< User intends to close the grip */
+        none /*!< No action detected */
+    }; /*!< Actions that can be detected */
 
-    Action readAction();
+    Action readAction(); /*!< discern current action @note currently must be polled in loop()*/
 
  private:
     const static unsigned int bufferSize = 40;

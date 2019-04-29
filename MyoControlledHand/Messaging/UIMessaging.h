11f356b3f65cf200a4484f5237c53742683e2662
prototriangle <prototriangle@gmail.com>
2019-04-29 08:09:17 +0100

    Add UIState class

--- MyoControlledHand/Messaging/UIMessaging.h
+++ MyoControlledHand/Messaging/UIMessaging.h
@@ -4,16 +4,27 @@
 #include "Arduino.h"
 #include "Messaging.h"
 
+class UIState {
+ public:
+    void selectGrip(unsigned int) {
+
+    }
+    unsigned int getSelectedGripIndex() {
+        return gripIndexSelected;
+    }
+ private:
+    unsigned int gripIndexSelected;
+};
+
 /*!
  * @brief Handle structured serial communication between Arduinos
  */
-
 class UIMessageHandler : public MessageHandler {
  public:
     UIMessageHandler();
  private:
     void interpretMessage(int length) final;
-
+    UIState *state;
 };
 
 #endif // UIMessaging_h

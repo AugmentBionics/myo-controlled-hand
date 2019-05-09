a0c6a5d6761cc99ebc2ae4b658230c2e6b09bbf8
prototriangle <prototriangle@gmail.com>
2019-05-09 13:04:43 +0100

    Remove old motor config and add current i2c config

--- MyoControlledHand/Config/Config.h
+++ MyoControlledHand/Config/Config.h
@@ -21,22 +21,12 @@
 #define M3_REV 10
 #define M3_DEL 11
 
-#define MOTOR_1_FWD_PIN 4
-#define MOTOR_1_REV_PIN 5
-#define MOTOR_1_CUR_PIN 6
+#define CURRENT_LIMITER_ADDR 0x2F
+#define CURRENT_LIMITER_M0 0x00
+#define CURRENT_LIMITER_M1 0x10
+#define CURRENT_LIMITER_M2 0x60
+#define CURRENT_LIMITER_M3 0x70
 
-#define MOTOR_2_FWD_PIN 7
-#define MOTOR_2_REV_PIN 8
-#define MOTOR_2_CUR_PIN 9
-
-#define MOTOR_3_FWD_PIN 10
-#define MOTOR_3_REV_PIN 11
-#define MOTOR_3_CUR_PIN 12
-
-#define MOTOR_4_FWD_PIN A0
-#define MOTOR_4_REV_PIN 2
-#define MOTOR_4_CUR_PIN A1
-#define MOTOR_LIM_PIN 3
 
 #define MYO_1_SIG_PIN A7
 #define MYO_1_RAW_PIN A6

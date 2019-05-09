a957690d6482bd590eb6586006ef93c3fff849d2
prototriangle <prototriangle@gmail.com>
2019-05-09 12:47:36 +0100

    Cut old motor controller code

--- MyoControlledHand/MotorController/MotorController.cpp
+++ MyoControlledHand/MotorController/MotorController.cpp
@@ -1,161 +1,52 @@
-#include "Arduino.h"
+#include <Grips.h>
 #include "MotorController.h"
 
-#define DEBUG_LOG(message) if(debug){Serial.println(message);}
-#define DEBUG_LOG_TUPLE(m1, m2) if(debug){Serial.print(m1); Serial.print(", "); Serial.println(m2);}
-
-//#define CURRENT_POLLING_RATE 10
-//static const uint16_t ocr1aValue = static_cast<const uint16_t>(62500.0f / float(CURRENT_POLLING_RATE));
-
-
-MotorController::MotorController(bool debug) : debug(debug) {
-    _currentGrip = {.name = "initial grip", .type = simple};
-}
-
-MotorController::MotorController() : MotorController(false) {
-}
-
-void MotorController::init(Actuator::Config configs[NUMBER_OF_ACTUATORS]) {
-    for (int i = 0; i < NUMBER_OF_ACTUATORS; i++) {
-        _actuators[i] = Actuator(configs[i]);
-    }
-    pinMode(MOTOR_LIM_PIN, OUTPUT);
-    digitalWrite(MOTOR_LIM_PIN, HIGH);
-}
+MotorController::MotorController() { _currentGrip = grips[0]; }
 
 void MotorController::setGrip(Grip grip) {
     _currentGrip = grip;
-    DEBUG_LOG("Set grip to ");
-    DEBUG_LOG(_currentGrip.name);
 
-    for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-        if (_currentGrip.actuationPattern[i] == ActuationScheme::close) {
-            _actuators[i].runForward();
-            DEBUG_LOG_TUPLE(_actuators[i].getName(), "FWD");
-        } else if (_currentGrip.actuationPattern[i] == ActuationScheme::open) {
-            _actuators[i].runReverse();
-            DEBUG_LOG_TUPLE(_actuators[i].getName(), "REV");
-        }
-    }
+}
 
-    uint16_t limitCounts[NUMBER_OF_ACTUATORS];
-    const uint16_t threshold = 200;
-    bool done = false;
-    while (!done) {
-        done = true;
-        for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-            if (_currentGrip.actuationPattern[i] == ActuationScheme::close
-                || _currentGrip.actuationPattern[i] == ActuationScheme::open) {
-                if (_actuators[i].isLimited()) {
-                    limitCounts[i] += 1;
-                } else {
-                    limitCounts[i] = 0;
-                }
-                done = done && limitCounts[i] > threshold;
+void MotorController::open(unsigned int i) {
+    regState = regState & ~mBrake;
+    regState = regState | (motorMasks[i] & mOpen);
+    shiftOutWord(regState);
+}
+
+void MotorController::close(unsigned int i) {
+    regState = regState & ~mBrake;
+    regState = regState | (motorMasks[i] & mClose);
+    shiftOutWord(regState);
+}
+
+void MotorController::brake(unsigned int i) {
+    regState = regState & ~mBrake;
+    regState = regState | (motorMasks[i] & mBrake);
+    shiftOutWord(regState);
+
+}
+
+void MotorController::idle(unsigned int i) {
+    //Not Implemented
+}
+
+bool MotorController::checkCurrentLimiting(unsigned int i) {
+    return false;
+}
+
+void MotorController::shiftOutWord(unsigned int w) {
+    for (unsigned int i = 0; i < 16; i++) {
+        if (bitRead(w, i) == true) {
+            if (bitRead(outState, i) == false) {
+                //pwm.setPin(i, 4095);
+            }
+        } else if (bitRead(w, i) == false) {
+            if (bitRead(outState, i) == true) {
+                //pwm.setPin(i, 0);
             }
         }
-
-        delay(5);
     }
+    outState = w;
 
-    for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-        if (_currentGrip.actuationPattern[i] == ActuationScheme::close
-            || _currentGrip.actuationPattern[i] == ActuationScheme::open) {
-            _actuators[i].brake();
-        }
-    }
-}
-
-bool MotorController::checkCurrentLimiting(uint16_t actuatorIndex) {
-    return checkCurrentLimiting(_actuators[actuatorIndex]);
-}
-
-bool MotorController::checkCurrentLimiting(Actuator actuator) {
-    return actuator.isLimited();
-}
-
-bool *MotorController::checkCurrentLimiting() {
-    for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-        _currentLimitingValues[i] = checkCurrentLimiting(i);
-    }
-    return _currentLimitingValues;
-}
-
-void MotorController::open() {
-    for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-        if (_currentGrip.actuationPattern[i] == ActuationScheme::actuate)
-            _actuators[i].runReverse();
-    }
-}
-void MotorController::test() {
-
-    const uint16_t threshold = 200;
-    for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-        Serial.println(_actuators[i].getName());
-        _actuators[i].runForward();
-
-        uint16_t limitCount = 0;
-        unsigned long t = 0;
-        bool done = false;
-        while (!done) {
-            done = true;
-            if (_actuators[i].isLimited()) {
-                limitCount += 1;
-            } else {
-                limitCount = 0;
-            }
-            done = done && limitCount > threshold;
-            Serial.println(limitCount);
-            delay(5);
-            if (++t > 1000)
-                break;
-        }
-
-        _actuators[i].brake();
-        delay(1300);
-        Serial.println(_actuators[i].getName());
-        _actuators[i].runReverse();
-
-        limitCount = 0;
-        done = false;
-        t = 0;
-        while (!done) {
-            done = true;
-            if (_actuators[i].isLimited()) {
-                limitCount += 1;
-            } else {
-                limitCount = 0;
-            }
-            done = done && limitCount > threshold;
-            Serial.println(limitCount);
-            delay(5);
-
-//            if (++t > 2000)
-//                break;
-        }
-
-        _actuators[i].brake();
-
-    }
-}
-
-void MotorController::close() {
-    for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-        if (_currentGrip.actuationPattern[i] == ActuationScheme::actuate)
-            _actuators[i].runForward();
-    }
-}
-
-void MotorController::brake() {
-    for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-        if (_currentGrip.actuationPattern[i] == ActuationScheme::actuate)
-            _actuators[i].brake();
-    }
-}
-
-void MotorController::coast() {
-    for (uint16_t i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
-        if (_currentGrip.actuationPattern[i] == ActuationScheme::actuate)
-            _actuators[i].coast();
-    }
 }
--- MyoControlledHand/MotorController/MotorController.h
+++ MyoControlledHand/MotorController/MotorController.h
@@ -2,36 +2,51 @@
 #define MotorController_h
 
 #include "Arduino.h"
-#include "Config.h"
-#include "Actuator.h"
-#include "GripUtil.h"
+#include "../Config/Config.h"
+#include "../GripUtil/GripUtil.h"
+#include <Adafruit_PWMServoDriver.h>
+
 
 /*!
  * Class that controls a number of Actuators.
  */
+
+#define del     0b0000100100100100
+#define mOpen   0b0000001001001001
+#define mClose  0b0000010010010010
+#define mBrake   0b0000011011011011
+
+#define m0Mask  0b0000000000000111
+#define m1Mask  0b0000000000111000
+#define m2Mask  0b0000000111000000
+#define m3Mask  0b0000111000000000
+
+#define NC      0b1111000000000000
+
 class MotorController {
  public:
+    explicit MotorController();
 
-    MotorController();
-    explicit MotorController(bool debug);
-
-    void init(Actuator::Config configs[NUMBER_OF_ACTUATORS]);
     void setGrip(Grip grip);
-    void open();
-    void close();
-    void brake();
-    void coast();
-    void test();
-    bool *checkCurrentLimiting();
-    bool debug;
-    bool checkCurrentLimiting(uint16_t actuatorIndex);
-    bool checkCurrentLimiting(Actuator actuator);
+    void open(unsigned int i);
+    void close(unsigned int i);
+    void brake(unsigned int i);
+    void idle(unsigned int i);
+    bool checkCurrentLimiting(unsigned int i);
 
  private:
 
+    Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+    void shiftOutWord(word w);
+
     Grip _currentGrip;
-    Actuator _actuators[NUMBER_OF_ACTUATORS];
     bool _currentLimitingValues[NUMBER_OF_ACTUATORS];
+
+    word regState = del;
+    word outState = 0;
+    const word motorMasks[NUMBER_OF_ACTUATORS] = {m0Mask, m1Mask, m2Mask, m3Mask};
+
 };
 
 #endif // MotorController_h

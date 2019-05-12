73dcc8c36826eb5794f291e8d444240fe73aaca8
prototriangle <prototriangle@gmail.com>
2019-05-13 00:16:52 +0100

    Update motor controller class with changes from 304c314

--- MyoControlledHand/MotorController/MotorController.cpp
+++ MyoControlledHand/MotorController/MotorController.cpp
@@ -1,40 +1,69 @@
-#include <Grips.h>
+#include "Arduino.h"
+#include "Config.h"
 #include "MotorController.h"
 
-MotorController::MotorController() {
-    _currentGrip = grips[0];
+MotorController::MotorController() {}
+
+void MotorController::init() {
     pwm.begin();
     pwm.setPWMFreq(24.0f);
     Wire.begin();
-}
-
-void MotorController::setGrip(Grip grip) {
-    _currentGrip = grip;
-
+    for (int i = 0; i < NUMBER_OF_ACTUATORS; ++i) {
+        setCurrentLimit(i, initCurrentLimits[i]);
+        setDelPin(i, INIT_DEL_PIN_VAL);
+        pinMode(rdPins[i], INPUT);
+    }
 }
 
 void MotorController::open(unsigned int i) {
-    pwm.setPin(reversePins[i], 4096);
+    if (isOpen[i])
+        return;
+    //Serial.print("MotorController::open\t");
+    //Serial.println(i);
+    setCurrentLimit(i, openCurrentLimits[i]);
+    pwm.setPin(reversePins[i], motorOpenSpeed[i]);
     pwm.setPin(forwardPins[i], 0);
+    lastInstruction[i] = MotorInstruction::open;
 }
 
 void MotorController::close(unsigned int i) {
+    //Serial.print("MotorController::close\t");
+    //Serial.println(i);
+    setCurrentLimit(i, closeCurrentLimits[i]);
+    pwm.setPin(reversePins[i], 0);
+    pwm.setPin(forwardPins[i], motorCloseSpeed[i]);
+    lastInstruction[i] = MotorInstruction::close;
+    isOpen[i] = false;
+}
+
+void MotorController::brake(unsigned int i, bool doNotLog) {
+    //Serial.print("MotorController::brake\t");
+    //Serial.println(i);
     pwm.setPin(reversePins[i], 4096);
-    pwm.setPin(forwardPins[i], 0);
+    pwm.setPin(forwardPins[i], 4096);
+    if (!doNotLog)
+        lastInstruction[i] = MotorInstruction::brake;
 }
 
 void MotorController::brake(unsigned int i) {
-    pwm.setPin(reversePins[i], 4096);
-    pwm.setPin(forwardPins[i], 4096);
+    brake(i, true);
+}
+
+void MotorController::idle(unsigned int i, bool doNotLog) {
+    //Serial.print("MotorController::idle\t");
+    //Serial.println(i);
+    pwm.setPin(reversePins[i], 0);
+    pwm.setPin(forwardPins[i], 0);
+    if (!doNotLog)
+        lastInstruction[i] = MotorInstruction::idle;
 }
 
 void MotorController::idle(unsigned int i) {
-    pwm.setPin(reversePins[i], 0);
-    pwm.setPin(forwardPins[i], 0);
+    idle(i, true);
 }
 
 bool MotorController::checkCurrentLimiting(unsigned int i) {
-    return false;
+    return digitalRead(rdPins[i]) == LOW;
 }
 
 void MotorController::setCurrentLimit(unsigned int i, unsigned int limit) {
@@ -43,3 +72,11 @@ void MotorController::setCurrentLimit(unsigned int i, unsigned int limit) {
     Wire.write(limit); //Current limiting value
     Wire.endTransmission();
 }
+
+void MotorController::setDelPin(unsigned int i, unsigned int value) {
+    pwm.setPin(delPins[i], value);
+}
+
+bool MotorController::isClosed[NUMBER_OF_ACTUATORS] = {};
+bool MotorController::isOpen[NUMBER_OF_ACTUATORS] = {};
+MotorInstruction MotorController::lastInstruction[NUMBER_OF_ACTUATORS];
--- MyoControlledHand/MotorController/MotorController.h
+++ MyoControlledHand/MotorController/MotorController.h
@@ -2,8 +2,7 @@
 #define MotorController_h
 
 #include "Arduino.h"
-#include "../Config/Config.h"
-#include "../GripUtil/GripUtil.h"
+#include "Config.h"
 #include <Adafruit_PWMServoDriver.h>
 
 
@@ -23,34 +22,64 @@
 
 #define NC      0b1111000000000000
 
+enum MotorInstruction {
+    none,
+    open,
+    close,
+    brake,
+    idle
+};
+
 class MotorController {
  public:
     explicit MotorController();
 
-    void setGrip(Grip grip);
+    void init();
+
     void open(unsigned int i);
     void close(unsigned int i);
+    void brake(unsigned int i, bool doNotLog);
+    void idle(unsigned int i, bool doNotLog);
     void brake(unsigned int i);
     void idle(unsigned int i);
     bool checkCurrentLimiting(unsigned int i);
 
+    static MotorInstruction lastInstruction[NUMBER_OF_ACTUATORS];
+    static bool isClosed[NUMBER_OF_ACTUATORS];
+    static bool isOpen[NUMBER_OF_ACTUATORS];
+
  private:
 
     Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-    Grip _currentGrip;
     bool _currentLimitingValues[NUMBER_OF_ACTUATORS];
 
-    word regState = del;
-    word outState = 0;
-    const word motorMasks[NUMBER_OF_ACTUATORS] = {m0Mask, m1Mask, m2Mask, m3Mask};
-
-    const unsigned int forwardPins[NUMBER_OF_ACTUATORS] = {M0_FWD, M1_FWD, M2_FWD, M3_FWD};
-    const unsigned int reversePins[NUMBER_OF_ACTUATORS] = {M0_REV, M1_REV, M2_REV, M3_REV};
-    const unsigned int delPins[NUMBER_OF_ACTUATORS] = {M0_DEL, M1_DEL, M2_DEL, M3_DEL};
+    const unsigned int
+        forwardPins[NUMBER_OF_ACTUATORS] = {M0_FWD, M1_FWD, M2_FWD, M3_FWD};
+    const unsigned int
+        reversePins[NUMBER_OF_ACTUATORS] = {M0_REV, M1_REV, M2_REV, M3_REV};
+    const unsigned int
+        delPins[NUMBER_OF_ACTUATORS] = {M0_DEL, M1_DEL, M2_DEL, M3_DEL};
     const unsigned int
         limPins[NUMBER_OF_ACTUATORS] = {CURRENT_LIMITER_M0, CURRENT_LIMITER_M1, CURRENT_LIMITER_M2, CURRENT_LIMITER_M3};
+    const unsigned int
+        rdPins[NUMBER_OF_ACTUATORS] = {M0_RD, M1_RD, M2_RD, M3_RD};
+    const unsigned int
+        motorCloseSpeed[NUMBER_OF_ACTUATORS] = {SPEED_CLOSE_M0, SPEED_CLOSE_M1, SPEED_CLOSE_M2, SPEED_CLOSE_M3};
+    const unsigned int
+        motorOpenSpeed[NUMBER_OF_ACTUATORS] = {SPEED_OPEN_M0, SPEED_OPEN_M1, SPEED_OPEN_M2, SPEED_OPEN_M3};
+    const unsigned int
+        initCurrentLimits[NUMBER_OF_ACTUATORS] =
+        {INIT_CURRENT_LIM_M0, INIT_CURRENT_LIM_M1, INIT_CURRENT_LIM_M2, INIT_CURRENT_LIM_M3};
+    const unsigned int
+        openCurrentLimits[NUMBER_OF_ACTUATORS] =
+        {OPEN_CURRENT_LIM_M0, OPEN_CURRENT_LIM_M1, OPEN_CURRENT_LIM_M2, OPEN_CURRENT_LIM_M3};
+    const unsigned int
+        closeCurrentLimits[NUMBER_OF_ACTUATORS] =
+        {CLOSE_CURRENT_LIM_M0, CLOSE_CURRENT_LIM_M1, CLOSE_CURRENT_LIM_M2, CLOSE_CURRENT_LIM_M3};
 
     void setCurrentLimit(unsigned int i, unsigned int limit);
+
+    void setDelPin(unsigned int i, unsigned int value);
 };
 
 #endif // MotorController_h

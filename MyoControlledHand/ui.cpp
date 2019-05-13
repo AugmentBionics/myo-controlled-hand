a4342ce50afddf4b3e874af8cb88b1bfbc4c5247
prototriangle <prototriangle@gmail.com>
2019-05-13 15:15:49 +0100

    Add long press and trigger on button up rather than down

--- MyoControlledHand/ui.cpp
+++ MyoControlledHand/ui.cpp
@@ -25,38 +25,55 @@ void setup() {
 }
 
 unsigned long lastChangeMillis = 0;
-unsigned long waitMillis = 1400;
+unsigned long button3PressStart = 0;
+const unsigned long longPress = 1000;
+const unsigned long waitMillis = 1400;
+
+volatile int lastButton1State;
+volatile int lastButton2State;
+volatile int lastButton3State;
 
 void loop() {
     // Handle serial instructions
     messageHandler.handleSerial();
 
+    const unsigned long t = millis();
+
     // Read from buttons
-    int button1 = digitalRead(BUTTON1_PIN);
-    int button2 = digitalRead(BUTTON2_PIN);
-    int button3 = digitalRead(BUTTON3_PIN);
+    int button1State = digitalRead(BUTTON1_PIN);
+    int button2State = digitalRead(BUTTON2_PIN);
+    int button3State = digitalRead(BUTTON3_PIN);
+
+    bool button1Up = button1State == HIGH && lastButton1State == LOW;
+    bool button2Up = button2State == HIGH && lastButton2State == LOW;
+    bool button3Up = button3State == HIGH && lastButton3State == LOW;
+    bool button3Down = button3State == LOW && lastButton3State == HIGH;
+
+    if (button3Down)
+        button3PressStart = t;
+
 
     // Select primary grip if button 3 is LOW
-    if (button3 == LOW) {
-        state.showGrip(state.primaryIndex);
-        lastChangeMillis = millis();
-        state.selectGrip();
-        messageHandler.sendCurrentGripSelection();
-        return;
-    }
-
-    // Update UI Selection
-    if (button1 == LOW && button2 == LOW) {
-        // Update primary
-        state.primaryIndex = state.getShownGripIndex();
-        state.showGrip(state.primaryIndex); //update 'P' symbol
-        lastChangeMillis = millis();
-    } else if (button1 == LOW && state.getShownGripIndex() < NUMBER_OF_PRIMARY_GRIPS - 1) {
-        state.showNextGrip();
-        lastChangeMillis = millis();
-    } else if (button2 == LOW && state.getShownGripIndex() > 0) {
-        state.showPreviousGrip();
-        lastChangeMillis = millis();
+    if (button3Up) {
+        if (t - button3PressStart > longPress) {
+            state.primaryIndex = state.getShownGripIndex();
+            state.showGrip(state.primaryIndex); //update 'P' symbol
+            lastChangeMillis = t;
+        } else {
+            state.showGrip(state.primaryIndex);
+            lastChangeMillis = t;
+        }
+    } else {
+        // Update UI Selection
+        if (button1Up && button2Up) {
+            // do nothing
+        } else if (button1Up && state.getShownGripIndex() < NUMBER_OF_PRIMARY_GRIPS - 1) {
+            state.showNextGrip();
+            lastChangeMillis = t;
+        } else if (button2Up == LOW && state.getShownGripIndex() > 0) {
+            state.showPreviousGrip();
+            lastChangeMillis = t;
+        }
     }
 
     // If enough time has passed then send grip selection to motor controller
@@ -64,4 +81,8 @@ void loop() {
         state.selectGrip();
         messageHandler.sendCurrentGripSelection();
     }
+
+    lastButton1State = button1State;
+    lastButton2State = button2State;
+    lastButton3State = button3State;
 }
